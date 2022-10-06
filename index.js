b2a281dbd318d13fd63b52341caedb826115cc96
SMN947 <betum98@gmail.com>
2022-10-05 22:11:15 -0500

    Añadida distancia a la visual

--- index.js
+++ index.js
@@ -27,39 +27,14 @@ function simulate() {
 		planet.y += Math.random() * (randSeed - -randSeed) + -randSeed;
 		return planet;
 	});
-	drawPlanets();
-
-	console.clear();
-	console.table(systemData);
-
-	// requestAnimationFrame(simulate);
-	setTimeout(() => {
-		simulate();
-	}, 1000);
-}
-
-let tsInicio = new Date();
-function drawPlanets() {
-	tsInicio = new Date();
-	planetsRelations = {};
-	universe.clearRect(0, 0, systemCanvas.width, systemCanvas.height);
+	//
 	planets.map((planet1, i) => {
 		planets.map((planet, j) => {
-			universe.beginPath();
-			universe.fillStyle = planet.color;
-			universe.arc(planet.x, planet.y, planet.size, 0, Math.PI * 2, false);
-			universe.fill();
-			universe.closePath();
-
 			// Trazo de lineas entre planetas
 			if (i != j && planetsRelations[`${i}-${j}`] == undefined) {
 				//Guarda relaciones para no procesar 2 veces
 				planetsRelations[`${i}-${j}`] = {d: 10};
 				planetsRelations[`${j}-${i}`] = {d: 10};
-
-				universe.moveTo(planet1.x, planet1.y);
-				universe.lineTo(planet.x, planet.y);
-				universe.stroke();
 
 				let midPointX = planet.x + (planet1.x - planet.x) * 0.5;
 				let midPointY = planet.y + (planet1.y - planet.y) * 0.5;
@@ -68,12 +43,66 @@ function drawPlanets() {
 				universe.fillRect(midPointX, midPointY, sizeFactor / 2, sizeFactor / 2);
 
 				systemData[`${j}-${i}`] = {
-					midPoint: `${midPointX.toFixed(2)}, ${midPointY.toFixed(2)}`,
+					midPoint: {
+						x: midPointX.toFixed(2),
+						y: midPointY.toFixed(2),
+					},
 					distance: calcDistance(planet.x, planet.y, planet1.x, planet1.y),
+					line: {
+						origin: {x: planet.x, y: planet.y},
+						destination: {x: planet1.x, y: planet1.y},
+					},
 				};
 			}
 		});
 	});
+
+	drawPlanets();
+	drawLines();
+
+	//console.clear();
+	//console.table(systemData);
+
+	// requestAnimationFrame(simulate);
+	setTimeout(() => {
+		simulate();
+	}, 2000);
+}
+
+let tsInicio = new Date();
+function drawPlanets() {
+	tsInicio = new Date();
+	planetsRelations = {};
+	universe.clearRect(0, 0, systemCanvas.width, systemCanvas.height);
+
+	planets.map((planet, i) => {
+		universe.beginPath();
+		universe.fillStyle = planet.color;
+		universe.arc(planet.x, planet.y, planet.size, 0, Math.PI * 2, false);
+		universe.fill();
+		universe.closePath();
+	});
+}
+
+function drawLines() {
+	for (relation of Object.keys(systemData)) {
+		let line = systemData[relation].line;
+		let distance = systemData[relation].distance;
+		let midPoint = systemData[relation].midPoint;
+		universe.moveTo(line.origin.x, line.origin.y);
+		universe.lineTo(line.destination.x, line.destination.y);
+		universe.stroke();
+
+		universe.textAlign = 'center';
+		universe.textBaseline = 'middle';
+		universe.font = '15px Courier';
+		let text = [distance, distance, 1, 2, 3, 4];
+
+		// draw text at the midpoint
+		universe.fillStyle = '#000';
+		for (var i = 0; i < text.length; i++)
+			universe.fillText(distance, midPoint.x, midPoint.y);
+	}
 }
 
 simulate();
@@ -82,7 +111,7 @@ function calcDistance(x1, y1, x2, y2) {
 	let a = x1 - x2;
 	let b = y1 - y2;
 
-	return Math.sqrt(a * a + b * b).toFixed(4);
+	return Math.sqrt(a * a + b * b).toFixed(2);
 }
 function getRndColor() {
 	var r = (255 * Math.random()) | 0,
